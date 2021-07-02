2eddb7964adfd8d0459b563e25fdd69db49d6aaf
VladimirMoor <81152453+VladimirMoor@users.noreply.github.com>
2021-07-02 15:36:39 +0300

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Vladimir on 01.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketList/ContentView.swift
@@ -0,0 +1,129 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Vladimir on 01.07.2021.
+//
+
+import SwiftUI
+import MapKit
+import LocalAuthentication
+
+struct ContentView: View {
+    @State private var centerCoordinate = CLLocationCoordinate2D()
+    @State private var locations = [CodableMKPointAnnotation]()
+    @State private var selectedPlace: MKPointAnnotation?
+    @State private var showingPlaceDetail = false
+    @State private var showingEditScreen = false
+    @State private var isUnlocked = false
+
+    var body: some View {
+        ZStack {
+            if isUnlocked {
+            MapView(selectedPlace: $selectedPlace, showingPlaceDetail: $showingPlaceDetail, centerCoordinate: $centerCoordinate, annotations: locations)
+                .edgesIgnoringSafeArea(.all)
+                
+            Circle()
+                .fill(Color.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        
+                        let newLocation = CodableMKPointAnnotation()
+                        newLocation.coordinate = self.centerCoordinate
+                        newLocation.title = "Example location"
+                        self.locations.append(newLocation)
+                        self.selectedPlace = newLocation
+                        self.showingEditScreen = true
+                        
+                    }) {
+                        Image(systemName: "plus")
+                            .padding()
+                            .background(Color.black.opacity(0.75))
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .clipShape(Circle())
+                            .padding(.trailing)
+                    }
+                    
+                }
+            }
+            } else {
+                Button("Unlock Places") {
+                    self.authenticate()
+                }
+                .padding()
+                .background(Color.black.opacity(0.75))
+                .foregroundColor(.white)
+                .clipShape(Capsule())
+            }
+        }.alert(isPresented: $showingPlaceDetail) {
+            Alert(title: Text(selectedPlace?.title ?? "Unknown"), message: Text(selectedPlace?.subtitle ?? "Missing place information"), primaryButton: .default(Text("OK")), secondaryButton: .default(Text("Edit")) {
+                self.showingEditScreen = true
+            } )
+        }
+        .sheet(isPresented: $showingEditScreen, onDismiss: saveData) {
+            if self.selectedPlace != nil {
+                EditView(placemark: self.selectedPlace!)
+            }
+        }
+        .onAppear(perform: loadData)
+        
+    }
+    
+    func getDocumentDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func loadData() {
+        let filename = getDocumentDirectory().appendingPathComponent("SavedPlaces")
+        
+        do {
+            let data = try Data(contentsOf: filename)
+            locations = try JSONDecoder().decode([CodableMKPointAnnotation].self, from: data)
+            
+        } catch {
+            print("Unable to load saved data")
+        }
+    }
+    
+    func saveData() {
+        do {
+            let filename = getDocumentDirectory().appendingPathComponent("SavedPlaces")
+            let data = try JSONEncoder().encode(self.locations)
+            try data.write(to: filename, options: [.atomicWrite, .completeFileProtection])
+        } catch {
+            print("Unable to save data.")
+        }
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Please authenticate yourself to unlock the places."
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                
+                DispatchQueue.main.async {
+                    if success {
+                        self.isUnlocked = true
+                    } else {
+                        // error
+                    }
+                }
+                
+            }
+        } else {
+            // no biometrics
+        }
+    }
+}
+
+
